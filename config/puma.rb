6327f6180cca2f135904b34338ab60f1da385060
Brett Posner <brett.posner@gmail.com>
2025-08-04 23:16:05 +0100

    First commit

---
+++ config/puma.rb
@@ -0,0 +1,24 @@
+# config/puma.rb
+
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+
+if ENV["RAILS_ENV"] == "production"
+  require "concurrent-ruby"
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  workers worker_count if worker_count > 1
+end
+
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+
+# ✅ HTTPS Puma server
+ssl_bind '0.0.0.0', '3000', {
+  key: "config/ssl/server.key",
+  cert: "config/ssl/server.crt",
+  verify_mode: "none"
+}
+
+environment ENV.fetch("RAILS_ENV") { "development" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+plugin :tmp_restart
